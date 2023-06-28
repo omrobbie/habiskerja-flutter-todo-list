2dfeab43ebaff53a1b9df1e2ef9a0a43cde25b65
omrobbie <putu.roby@yahoo.com>
2023-06-28 22:25:03 +0700

    prepare network manager

---
+++ lib/utils/network_manager.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:todo_list/models/todo_item.dart';
+
+class NetworkManager {
+  late final Dio _dio;
+  final baseUrl = 'http://localhost:3000';
+
+  NetworkManager() {
+    _dio = Dio();
+  }
+
+  Future<List<TodoItem>> getTodosIsDone(bool isDone) async {
+    final result = await _dio.get(
+      '$baseUrl/todos?isDone=$isDone',
+    );
+
+    return (result.data as List).map((e) => TodoItem.fromMap(e)).toList();
+  }
+
+  Future<TodoItem> postData(TodoItem item) async {
+    final result = await _dio.post(
+      '$baseUrl/todos',
+      data: item.toMap(),
+    );
+
+    return TodoItem.fromMap(result.data);
+  }
+
+  Future<TodoItem> updateData(TodoItem item) async {
+    final result = await _dio.put(
+      '$baseUrl/todos/${item.id}',
+      data: item.toMap(),
+    );
+
+    return TodoItem.fromMap(result.data);
+  }
+
+  Future<void> deleteData(TodoItem item) async {
+    await _dio.delete(
+      '$baseUrl/todos/${item.id}',
+    );
+  }
+}
